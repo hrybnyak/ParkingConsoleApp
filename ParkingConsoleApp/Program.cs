588d710d6649aa5c9cc0d6fb5fdac008f907ff64
hrybnyak <hrybnyak.y@gmail.com>
2019-05-19 12:46:44 +0300

    Add project files.

---
+++ ParkingConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ParkingConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
